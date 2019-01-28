913f9cec8d665d44e95c8499e07b951705aafffb
Vladimír Ulman <ulman@mpi-cbg.de>
2019-01-28 12:15:08 +0100

    CHG: testStreams.java extended to cover most imglib2 backends

--- src/test/java/de/mpicbg/ulman/imgstreamer/testStreams.java
+++ src/test/java/de/mpicbg/ulman/imgstreamer/testStreams.java
@@ -6,18 +6,33 @@ import net.imagej.ImgPlus;
 import net.imglib2.Cursor;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.planar.PlanarImgFactory;
+import net.imglib2.img.cell.CellImgFactory;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 
 public class testStreams
 {
 	public static void main(String... args)
 	{
-		testImgPlus();
 		//testQueingOfObjectsInAStream();
+		testImgPlus( fillImg(
+			new ArrayImgFactory<>(new UnsignedShortType()).create(200,100,5)
+		));
+
+		testImgPlus( fillImg(
+			new PlanarImgFactory<>(new UnsignedShortType()).create(200,100,5)
+		));
+
+		testImgPlus( fillImg(
+				new CellImgFactory<>(new UnsignedShortType(), 50,20,5).create(200,100,5)
+		));
 	}
 
-	static
-	void testImgPlus()
+
+	static <T extends NativeType<T>>
+	void testImgPlus(final Img<T> img)
 	{
 		class myLogger implements ProgressCallback
 		{
@@ -32,28 +47,32 @@ public class testStreams
 		}
 
 		try {
-			ImgPlus<UnsignedShortType> imgP = new ImgPlus<>( testStreams.createFakeImg() );
-			final OutputStream os = new FileOutputStream("/tmp/out.dat");
+			//create ImgPlus out of the input Img
+			ImgPlus<T> imgP = new ImgPlus<>(img);
 
+			//sample output stream
+			final OutputStream os = new FileOutputStream("/tmp/out.dat");
+			//
+			//ImgStreamer must always be instantiated if you want to know the complete
+			//stream size before the actual serialization into an OutputStream
 			ImgStreamer isv = new ImgStreamer( new myLogger() );
 			isv.setImageForStreaming(imgP);
 			System.out.println("stream length will be: "+isv.getOutputStreamLength());
 			isv.write(os);
-
+			//
 			os.close();
 
+			//sample input stream
 			final InputStream is = new FileInputStream("/tmp/out.dat");
-
+			//
+            //ImgStreamer needs not be instantiated to obtain an img from a stream
 			ImgPlus<?> imgPP = isv.read(is);
-
+			//
 			is.close();
 			System.out.println("got this image: "+imgPP.getImg().toString());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 
@@ -76,16 +95,14 @@ public class testStreams
 	}
 
 
-	static
-	Img<UnsignedShortType> createFakeImg()
+	static <T extends RealType<T>>
+	Img<T> fillImg(final Img<T> img)
 	{
-		Img<UnsignedShortType> img
-				= new ArrayImgFactory<UnsignedShortType>().create(new long[] {200,100,1}, new UnsignedShortType());
+		long counter = 0;
 
-		Cursor<UnsignedShortType> imgC = img.cursor();
-		int counter = 0;
+		Cursor<T> imgC = img.cursor();
 		while (imgC.hasNext())
-			imgC.next().set(counter++);
+			imgC.next().setReal(counter++);
 
 		return img;
 	}
