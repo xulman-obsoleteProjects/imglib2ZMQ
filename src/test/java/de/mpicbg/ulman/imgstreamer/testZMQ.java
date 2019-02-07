dd8f6f9783bf38cdc913423979f5bdd6e03cb5b1
Vladimír Ulman <ulman@mpi-cbg.de>
2019-02-07 21:54:44 +0100

    CHG: testZMQ tests ZeroMQInputStream.read() and ZeroMQInputStream.read(byte[X]),      that is, it tests non-buffered and buffered read of the stream

--- src/test/java/de/mpicbg/ulman/imgstreamer/testZMQ.java
+++ src/test/java/de/mpicbg/ulman/imgstreamer/testZMQ.java
@@ -8,6 +8,15 @@ public class testZMQ
 {
 	public static void main(String... args)
 	{
+		System.out.println("-------------------------------------------------");
+		testNonBufferedReadOut();
+
+		System.out.println("-------------------------------------------------");
+		testBufferedReadOut();
+	}
+
+	public static void testNonBufferedReadOut()
+	{
 		try {
 			ZMQ.Context zmqContext = ZMQ.context(1);
 			ZMQ.Socket zmqSocket = zmqContext.socket(ZMQ.PAIR);
@@ -31,8 +40,50 @@ public class testZMQ
 			} while (nextVal > -1);
 
 			zmqSocket.close();
+			zis.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-		} catch (IOException e) {
+	public static void testBufferedReadOut()
+	{
+		try {
+			ZMQ.Context zmqContext = ZMQ.context(1);
+			ZMQ.Socket zmqSocket = zmqContext.socket(ZMQ.PAIR);
+			zmqSocket.connect("tcp://localhost:3456");
+
+			byte[] testSendArray = new byte[] { 97,120,98,99,100,97,98,99,100,97,98,99 };
+			zmqSocket.send(testSendArray);
+
+			byte[] testRecvArray = new byte[5];
+
+			final ZeroMQInputStream zis = new ZeroMQInputStream(3456, 10);
+			int nextVal = 0;
+			do {
+				int itemsRead = zis.read(testRecvArray);
+				nextVal = itemsRead;
+				System.out.println("itemsRead="+itemsRead);
+
+				for (int q=0; q < itemsRead; ++q)
+				{
+					nextVal = testRecvArray[q];
+					System.out.println("got now this value: "+nextVal);
+
+					if (nextVal == 120)
+					{
+						//modify the buffer to recognize it...
+						for (int i=0; i < testSendArray.length; ++i) testSendArray[i]+=4;
+						zmqSocket.send(testSendArray);
+					}
+				}
+			} while (nextVal > -1);
+
+			zmqSocket.close();
+			zis.close();
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
