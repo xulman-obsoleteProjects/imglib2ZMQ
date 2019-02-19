9f503b3e77e1fbeb90ae23d503c7de8c2f122602
Vladimír Ulman <ulman@mpi-cbg.de>
2019-02-19 22:36:35 +0100

    ADD: license headers/preambles to all source code files

--- src/main/java/de/mpicbg/ulman/imgstreamer/ZeroMQInputStream.java
+++ src/main/java/de/mpicbg/ulman/imgstreamer/ZeroMQInputStream.java
@@ -1,3 +1,10 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
 package de.mpicbg.ulman.imgstreamer;
 
 import java.io.IOException;
